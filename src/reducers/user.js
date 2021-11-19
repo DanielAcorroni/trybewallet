7d0ca4b789f0424ce7443e128b8bd5c3d54bf169
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-19 16:39:02 -0300

    Cria reducer utilizado na página de login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_TYPE } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER_TYPE:
+    return ({
+      ...state,
+      email: action.payload,
+    });
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
