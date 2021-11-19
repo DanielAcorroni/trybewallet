005c7bb1622a3af132f5b6185c4bfdb1639f9292
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-19 18:10:05 -0300

    Incopora header ao wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    console.log(email);
+    return (
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+function mapStateToProps(state) {
+  const { user: { email } } = state;
+  return {
+    email,
+  };
+}
+
+export default connect(mapStateToProps, null)(Wallet);
