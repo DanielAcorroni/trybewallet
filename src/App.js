79191559d953955b2bb4805da6344b3b820a82ff
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-19 16:53:03 -0300

    Adiciona rota para componente Wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route component={ Login } exact path="/" />
+        <Route component={ Wallet } exact path="/carteira" />
       </Switch>
     );
   }
