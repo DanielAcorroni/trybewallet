84a9066708c9089dae79f1737d9f9ab251853dfc
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-23 18:35:35 -0300

    Adiciona actions da requisição de api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,23 @@
+import getCurrentCoins from '../services/requestCurrrentCoinAPI';
+
 export const USER_TYPE = 'USER_TYPE';
+export const WALLET_TYPE = 'WALLET_TYPE';
+
+export const LOADING_TYPE = 'LOADING_TYPE';
+export const SUCCES_TYPE = 'SUCCES_TYPE';
+export const ERROR_TYPE = 'ERROR_TYPE';
 
 export const userAction = (state) => ({ type: USER_TYPE, payload: state });
+export const walletAction = (state) => ({ type: WALLET_TYPE, payload: state });
+
+export const loadingAction = () => ({ type: LOADING_TYPE });
+export const succesApiAction = (state) => ({ type: SUCCES_TYPE, payload: state });
+export const errorApiAction = (error) => ({ type: ERROR_TYPE, payload: error });
+
+export const fetchApiWithThunk = () => (dispatch) => {
+  dispatch(loadingAction);
+
+  return getCurrentCoins()
+    .then((response) => dispatch(succesApiAction(response)),
+      () => dispatch(errorApiAction));
+};
