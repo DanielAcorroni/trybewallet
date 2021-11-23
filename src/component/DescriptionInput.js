51e396ae5065ad1fbaff0aca3e6368e67def4d74
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-23 18:34:57 -0300

    Adiciona componente description input

---
+++ src/component/DescriptionInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class DescriptionInput extends React.Component {
+  render() {
+    const { descript, handleChange } = this.props;
+    return (
+      <label htmlFor="description">
+        <textarea
+          data-testid="description-input"
+          id="description"
+          name="description"
+          onChange={ handleChange }
+          value={ descript }
+        />
+      </label>
+    );
+  }
+}
+
+DescriptionInput.propTypes = {
+  descript: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default DescriptionInput;
