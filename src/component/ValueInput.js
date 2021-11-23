6d3d7960ae4da77e05fe10461845dd206a4c9657
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-23 18:36:29 -0300

    Adiciona componente do value input

---
+++ src/component/ValueInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ValueInput extends React.Component {
+  render() {
+    const { value, handleChange } = this.props;
+    return (
+      <label htmlFor="value">
+        <input
+          type="number"
+          data-testid="value-input"
+          id="value"
+          name="value"
+          onChange={ handleChange }
+          value={ value }
+        />
+      </label>
+    );
+  }
+}
+
+ValueInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default ValueInput;
