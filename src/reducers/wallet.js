c8ae5ef589c9e0b54561f8931b672278b477371a
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-23 18:37:41 -0300

    Implementa reducer da wallet e da API

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,39 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ERROR_TYPE, LOADING_TYPE, SUCCES_TYPE, WALLET_TYPE } from '../actions';
+
+const INITIAL_STATE = {
+  expenses: [],
+  isLoading: false,
+  currencies: [],
+  error: '',
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case WALLET_TYPE:
+    return ({
+      ...state,
+      expenses: action.payload,
+    });
+  case LOADING_TYPE:
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case SUCCES_TYPE:
+    return {
+      ...state,
+      currencies: [action.payload],
+      isLoading: false,
+    };
+  case ERROR_TYPE:
+    return {
+      ...state,
+      error: action.payload,
+      isLoading: false,
+    };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
