7b30b4823324ed15550aa33dc67ac76a3ae70776
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-23 20:31:04 -0300

    Adiciona função de adicionar itens a tabela

--- src/component/TableWallet.js
+++ src/component/TableWallet.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class TableWallet extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -17,14 +20,49 @@ class TableWallet extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>January</td>
-            <td>$100</td>
-          </tr>
+          {
+            expenses.map((expense) => {
+              console.log(expense);
+              let totalValue = 0;
+              const { exchangeRates, currency, value } = expense;
+              const actualExchange = exchangeRates[currency];
+              const { ask, name } = actualExchange;
+              const separateName = name.split('/');
+              totalValue += (Number(value) * ask);
+              return (
+                <tr key={ expense.id }>
+                  <td role="cell">{ totalValue.toFixed(2) }</td>
+                  <td role="cell">{ expense.value }</td>
+                  <td role="cell">{ separateName[0] }</td>
+                  <td role="cell">{ expense.method }</td>
+                  <td role="cell">{ expense.description }</td>
+                  <td role="cell">{ expense.tag }</td>
+                  <td role="cell">{ Number(ask).toFixed(2) }</td>
+                  <td role="cell">Real</td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
     );
   }
 }
 
-export default TableWallet;
+TableWallet.defaultProps = {
+  expenses: [],
+};
+
+TableWallet.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object),
+};
+
+function mapStateToProps(state) {
+  const { wallet: { expenses } } = state;
+  return {
+    expenses,
+    state,
+  };
+}
+
+export default connect(mapStateToProps, null)(TableWallet);
