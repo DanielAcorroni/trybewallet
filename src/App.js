bf0181f28ee80f75625e128a454358ee43d3ab16
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-30 16:48:01 -0300

    Add config para funcionar gh pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route component={ Login } exact path="/" />
-        <Route component={ Wallet } path="/carteira" />
-      </Switch>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route component={ Login } exact path="/" />
+          <Route component={ Wallet } path="/carteira" />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
