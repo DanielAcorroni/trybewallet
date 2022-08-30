ebeb2cdd88968c830a3df04595d827bcb4cbc5a9
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-30 16:49:02 -0300

    Updates

---
+++ precache-manifest.0a1d1cfabbf76295557fa172df3f2acf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea0a9b759eda44fb7dd271a3cb4b33c",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "fd5eca440d2bc39f0175",
+    "url": "/trybewallet/static/css/main.8a8abba5.chunk.css"
+  },
+  {
+    "revision": "1c460a222b9bb026d1e8",
+    "url": "/trybewallet/static/js/2.ab889f15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.ab889f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5eca440d2bc39f0175",
+    "url": "/trybewallet/static/js/main.e00dd262.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "96a5eb93050e7f901ad16ab087cb72fd",
+    "url": "/trybewallet/static/media/money-logo.96a5eb93.png"
+  },
+  {
+    "revision": "6f60677fe07fe1e7e05c2bf6f89277e7",
+    "url": "/trybewallet/static/media/trybe-logo.6f60677f.png"
+  }
+]);
